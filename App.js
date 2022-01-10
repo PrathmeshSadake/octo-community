cefee65a6661ce7ae1c5ac25b634f17f570f66da
Prathmesh Sadake <prathmeshsadake@gmail.com>
2022-01-10 10:10:35 +0530

    expo app setup

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
@@ -13,8 +13,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCpAnuyZSkOL0P1nZCIMIr2cj5w5SaDWlw",
+//   authDomain: "octochat-0410.firebaseapp.com",
+//   projectId: "octochat-0410",
+//   storageBucket: "octochat-0410.appspot.com",
+//   messagingSenderId: "732759058767",
+//   appId: "1:732759058767:web:1048c5545a9c43167f7d3a"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
